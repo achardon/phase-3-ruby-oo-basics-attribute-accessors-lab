93b182633f4283237528fc8c7cdd9901926c7e36
Alessandra Chardon <achardon@ucla.edu>
2021-11-24 15:47:35 -0500

    Completed assignment

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,19 @@
-## code your solution here. 
+
+class Cat
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+
+    def meow
+        puts "meow!"
+    end
+end
+
+cat1 = Cat.new('Tigger')
+
+cat1.meow
+
+puts cat1.name
+
